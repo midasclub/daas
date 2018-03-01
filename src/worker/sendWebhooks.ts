f3579fe6bddc5c865cf16964b313fbae85b6e73f
Pablo Rodríguez <pabloviolin8@gmail.com>
2018-03-01 17:59:47 +0100

    Progress

--- src/worker/sendWebhooks.ts
+++ src/worker/sendWebhooks.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch"
-import { retry } from "async-retry"
+// import * as retry from "async-retry"
 import { Lobby, WebHook, WebHookEventType } from "@daas/model"
 import { WebHooks } from "@daas/db-adapter"
 
@@ -14,7 +14,7 @@ export async function sendWebhooks(
 
 async function sendWebhook(webhook: WebHook, lobby: Lobby, data: any) {
 	try {
-		await retry(
+		/*await retry(
 			() =>
 				fetch(webhook.url, {
 					method: "POST",
@@ -31,7 +31,29 @@ async function sendWebhook(webhook: WebHook, lobby: Lobby, data: any) {
 			{
 				retries: 10
 			}
-		)
+		)*/
+
+		// TODO add retry support
+		// Disabled right now because of an error in typings:
+		// The typings assume:
+		// import {retry} from "async-retry"
+		// But it's:
+		// import * as retry from "async-retry"
+		// Source of the typings
+		// https://github.com/DefinitelyTyped/DefinitelyTyped/tree/4d1d1b5ecbec8b111457bc9bbbe2ef0a7a3640ed/types/async-retry
+
+		await fetch(webhook.url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"X-DaaS-Webhook-Secret": webhook.secret
+			},
+			body: JSON.stringify({
+				webhook: webhook.serialize(),
+				lobby: lobby.serialize(),
+				data
+			})
+		})
 
 		console.log(`Sent ${WebHookEventType[webhook.eventType]} webhook to ${webhook.url}`)
 	} catch (e) {
